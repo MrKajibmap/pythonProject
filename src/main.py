9e025e4420141dbcee342fcefc0e75a2c4ae87e2
nb <nik.borz@mail.ru>
2022-04-13 17:43:58 +0300

    stable version 4, initial parse of tables_nms, libnames, job_nm, mapping

--- src/main.py
+++ src/main.py
@@ -14,30 +14,39 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     dict_of_key_words = ['etl_job_start', 'etl_job_finish']
-    dict_of_key_words_mapping = ['_OUTPUT_col\d+_name']
+    dict_of_key_words_mapping = ['\Wlet _OUTPUT_col\d{1,}_name']
     dict_job_nm = ['etls_jobName']
     dict_libname = ['LIBNAME']
     dict_input_table = ['_INPUT =']
     dict_output_table = ['_OUTPUT =']
+    libraries = []
     with open('./src/files/101_138_02_Extract_DGP_DWH_TRN_TYPE.sas', 'r', encoding='utf-16') as deploy:
         for line in deploy:
-            # колонки таблицы источника и таблицы _FULL
-            for i in range(0, len(dict_of_key_words_mapping)):
-                if re.search(dict_of_key_words_mapping[i], line):
-                    print(line.split(sep='=')[-1].split(sep=';')[0].strip())
-                # job_nm
-            if re.search(dict_job_nm[i], line):
+            # job_nm
+            if re.search(dict_job_nm[0], line):
                 print(line.split(sep='=')[-1].split(sep='%nrquote(')[-1].split(sep=');')[0].strip())
             # libnames
             if re.search(dict_libname[0], line):
-                print(line)
-                for j in range(0, line.count(' ')+1):
+                for j in range(0, line.count(' ') + 1):
                     if len(line.split(sep=';')[0].split(sep=' ')[j].strip()) > 0:
+                        libraries.append(line.split(sep=' ')[j].split(sep=';')[0].strip())
                         print(line.split(sep=' ')[j].split(sep=';')[0].strip())
-            #input table
+            # input table
             if re.search(dict_input_table[0], line):
-                print(line.split(sep='=')[-1].split(sep=';')[0].split(sep='.')[-1].strip())
+                print('input_table_name = ', line.split(sep='=')[-1].split(sep=';')[0].split(sep='.')[-1].strip())
             # output table
             if re.search(dict_output_table[0], line):
-                print(line.split(sep='=')[-1].split(sep=';')[0].split(sep='.')[-1].strip())
-
+                print('output_table_name = ', line.split(sep='=')[-1].split(sep=';')[0].split(sep='.')[-1].strip())
+            # колонки таблицы источника и таблицы _FULL
+            if re.search(dict_of_key_words_mapping[0], line):
+                print('ClmnPos = ', line.split(sep='col')[1].split(sep='_')[0].strip(), 'clmnNm = ', line.split(sep='=')[-1].split(sep=';')[0].strip())
+            if re.search('\Wlet _OUTPUT_col\d{1,}_length =', line):
+                print('ClmnPos = ', line.split(sep='col')[1].split(sep='_')[0].strip(), 'length = ',
+                      line.split(sep='=')[-1].strip())
+            if re.search('\Wlet _OUTPUT_col\d{1,}_type', line):
+                print('ClmnPos = ', line.split(sep='col')[1].split(sep='_')[0].strip(), 'type = ',
+                      line.split(sep='=')[-1].strip())
+            if re.search('\Wlet _OUTPUT_col\d{1,}_label =', line):
+                print('ClmnPos = ', line.split(sep='col')[1].split(sep='_')[0].strip(), 'label = ',
+                      line.split(sep='=')[-1].split(sep=')')[0].split(sep='(')[-1].strip())
+    print(list(libraries))
