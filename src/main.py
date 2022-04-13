2c76c7cb8e622e1867364ad4cfccd2857a6a96f2
nb <nik.borz@mail.ru>
2022-04-13 18:38:28 +0300

    перезаписывает список

--- src/main.py
+++ src/main.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
     dict_input_table = ['_INPUT =']
     dict_output_table = ['_OUTPUT =']
     libraries = []
+    clmnFull = {}
     with open('./src/files/101_138_02_Extract_DGP_DWH_TRN_TYPE.sas', 'r', encoding='utf-16') as deploy:
         for line in deploy:
             # job_nm
@@ -39,14 +40,33 @@ if __name__ == '__main__':
                 print('output_table_name = ', line.split(sep='=')[-1].split(sep=';')[0].split(sep='.')[-1].strip())
             # колонки таблицы источника и таблицы _FULL
             if re.search(dict_of_key_words_mapping[0], line):
-                print('ClmnPos = ', line.split(sep='col')[1].split(sep='_')[0].strip(), 'clmnNm = ', line.split(sep='=')[-1].split(sep=';')[0].strip())
+                # print('first input of clmn num = ', line.split(sep='col')[1].split(sep='_')[0].strip())
+                # print('ClmnPos = ', line.split(sep='col')[1].split(sep='_')[0].strip(), 'clmnNm = ',
+                #       line.split(sep='=')[-1].split(sep=';')[0].strip())
+                # clmnNm = {'clmnNm': line.split(sep='=')[-1].split(sep=';')[0].strip()}
+                # print(clmnNm)
+                clmnFull['clmnNm'] = line.split(sep='=')[-1].split(sep=';')[0].strip()
+                print(clmnFull)
             if re.search('\Wlet _OUTPUT_col\d{1,}_length =', line):
-                print('ClmnPos = ', line.split(sep='col')[1].split(sep='_')[0].strip(), 'length = ',
-                      line.split(sep='=')[-1].strip())
+                # print('ClmnPos = ', line.split(sep='col')[1].split(sep='_')[0].strip(), 'length = ',
+                #       line.split(sep='=')[-1].strip())
+                # clmnLngth = {'clmnLngth': line.split(sep='=')[-1].strip()}
+                # print(clmnLngth)
+                clmnFull['clmnLngth'] = line.split(sep='=')[-1].strip()
+                print(clmnFull)
             if re.search('\Wlet _OUTPUT_col\d{1,}_type', line):
-                print('ClmnPos = ', line.split(sep='col')[1].split(sep='_')[0].strip(), 'type = ',
-                      line.split(sep='=')[-1].strip())
+                # print('ClmnPos = ', line.split(sep='col')[1].split(sep='_')[0].strip(), 'type = ',
+                #       line.split(sep='=')[-1].split(sep=';')[0].strip())
+                # clmnType = {'clmnType': line.split(sep='=')[-1].split(sep=';')[0].strip()}
+                # print(clmnType)
+                clmnFull['clmnType'] = line.split(sep='=')[-1].split(sep=';')[0].strip()
+                print(clmnFull)
             if re.search('\Wlet _OUTPUT_col\d{1,}_label =', line):
-                print('ClmnPos = ', line.split(sep='col')[1].split(sep='_')[0].strip(), 'label = ',
-                      line.split(sep='=')[-1].split(sep=')')[0].split(sep='(')[-1].strip())
+                # print('ClmnPos = ', line.split(sep='col')[1].split(sep='_')[0].strip(), 'label = ',
+                #       line.split(sep='=')[-1].split(sep=')')[0].split(sep='(')[-1].strip())
+                # clmnLabel = {'clmnType': line.split(sep='=')[-1].split(sep=')')[0].split(sep='(')[-1].strip()}
+                # print(clmnLabel)
+                clmnFull['clmnLabel'] = line.split(sep='=')[-1].split(sep=')')[0].split(sep='(')[-1].strip()
+                print(clmnFull)
+
     print(list(libraries))
