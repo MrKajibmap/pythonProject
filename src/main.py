519c52872eaa8fdc2801ead7e6e5c713a13202cf
nb <nik.borz@mail.ru>
2022-04-18 19:15:05 +0300

    full json defined

--- src/main.py
+++ src/main.py
@@ -10,18 +10,48 @@ if __name__ == '__main__':
     dict_input_table = ['_INPUT =']
     dict_output_table = ['_OUTPUT =']
     libraries = []
+    taskExtractTmplt = {
+        "tasks": [
+            {
+                "task_id": "",
+                "description": "extract ...",
+                "type": "stg_oracle_odbc",
+                "source": [{
+                    "connection": "%s_prod_conn",
+                    "path": "fcchs",
+                    "entity": "...",
+                    "alias": "..._src"
+                }],
+                "target": [
+                    {
+                        "connection": "edw_hdfs",
+                        "path": "stg/raw/dwh",
+                        "entity": "...",
+                        "alias": "..._stg"
+                    }],
+                "mapping": []
+            }]
+    }
     clmnFullTmplt = {'mapping': [], 'tables': [], 'librefs': []}
     clmnSimpleTmplt = {
         "serial": "",
         "target": "",
-        "length": "",
+        "expression": "",
         "type": "",
         "label": "",
-        "bk": ""
+        "bk": "",
+        "source": [{
+            "serial": "1",
+            "alias": "*_src",
+            "column": "id"
+        }]
     }
     clmnFull = copy.deepcopy(clmnFullTmplt)
     clmnSimple = copy.deepcopy(clmnSimpleTmplt)
-    clmnsGeneral = {}
+    taskSimple = copy.deepcopy(taskExtractTmplt)
+    taskSimple['tasks'][0]['description'] = 'extract '
+    taskSimple['tasks'][0]['type'] = 'stg_oracle_odbc '
+    taskSimple['tasks'][0]['task_id'] = '1'
     with open('./src/files/input/101_138_02_Extract_DGP_DWH_TRN_TYPE.sas', 'r', encoding='utf-16') as deploy:
         for line in deploy:
             # initial new dict
@@ -30,10 +60,28 @@ if __name__ == '__main__':
                 print(line.split(sep='=')[-1].split(sep='%nrquote(')[-1].split(sep=');')[0].strip())
             # libnames
             if re.search(dict_libname[0], line):
+                # newList = line.split()
+                print(">>>> size of : \"", line.split(';')[0].strip().replace('  ', ' '), '" = ', len(line.strip().replace('  ', ' ').split(sep=' ')))
+                match line.split(';')[0].split():
+                    case operator, libref, type_conn, path, schema, authdomain:
+                        print(f'{operator=}, {libref=}, {type_conn=}, {path=}')
+                        taskSimple['tasks'][0]['source'][0]['connection'] = libref
+                        taskSimple['tasks'][0]['source'][0]['path'] = path.split('"')[1]
+                        taskSimple['tasks'][0]['source'][0]['entity'] = type_conn
+                        taskSimple['tasks'][0]['source'][0]['alias'] = libref+'_src'
+                    case operator, libref, type_conn, path:
+                        print(f'{operator=}, {libref=}, {type_conn=}, {path=}')
+                        taskSimple['tasks'][0]['target'][0]['connection'] = libref
+                        taskSimple['tasks'][0]['target'][0]['path'] = path.split('"')[1]
+                        taskSimple['tasks'][0]['target'][0]['entity'] = type_conn
+                        taskSimple['tasks'][0]['target'][0]['alias'] = libref
+                    case _:
+                        print('wrong libname operator definition')
                 for j in range(0, line.count(' ') + 1):
                     if len(line.split(sep=';')[0].split(sep=' ')[j].strip()) > 0:
                         libraries.append(line.split(sep=' ')[j].split(sep=';')[0].strip())
                         print(line.split(sep=' ')[j].split(sep=';')[0].strip())
+                # 3 element from each libname is path
             # input table
             if re.search(dict_input_table[0], line):
                 print('input_table_name = ', line.split(sep='=')[-1].split(sep=';')[0].split(sep='.')[-1].strip())
@@ -43,24 +91,29 @@ if __name__ == '__main__':
             # колонки таблицы источника и таблицы _FULL
             if re.search(dict_of_key_words_mapping[0], line):
                 clmnSimple['target'] = line.split(sep='=')[-1].split(sep=';')[0].strip()
-                clmnSimple['serial'] = str(int(line.split(sep='col')[-1].split(sep='_')[0].strip())+1)
+                clmnSimple['expression'] = line.split(sep='=')[-1].split(sep=';')[0].strip()
+                clmnSimple['serial'] = str(int(line.split(sep='col')[-1].split(sep='_')[0].strip()) + 1)
+                clmnSimple['source'][0]['serial'] = str(int(line.split(sep='col')[-1].split(sep='_')[0].strip()) + 1)
+                clmnSimple['source'][0]['alias'] = clmnSimple['target'] + '_src'
             if re.search('\Wlet _OUTPUT_col\d{1,}_length =', line):
                 clmnSimple['length'] = line.split(sep='=')[-1].split(sep=';')[0].strip()
             if re.search('\Wlet _OUTPUT_col\d{1,}_type', line):
-                # clmnSimple['type'] = line.split(sep='=')[-1].split(sep=';')[0].strip()
                 if line.split(sep='=')[-1].split(sep=';')[0].strip() == '$':
-                    clmnSimple['type'] = 'char'+'('+clmnSimple.get('length')+')'
+                    clmnSimple['type'] = 'char' + '(' + clmnSimple.get('length') + ')'
                 else:
                     clmnSimple['type'] = 'numeric'
             if re.search('\Wlet _OUTPUT_col\d{1,}_label =', line):
                 clmnSimple['label'] = line.split(sep='=')[-1].split(sep=')')[0].split(sep='(')[-1].strip()
+                # delete extra fields not used in future
                 del clmnSimple['length']
+                # dummy for bk-key = 'false'
+                clmnSimple['bk'] = 'false'
                 clmnToAppend = copy.deepcopy(clmnSimple)
                 clmnFull['mapping'].append(clmnToAppend)
-                print('>>>>>>', clmnFull)
+                taskSimple['tasks'][0]['mapping'].append(clmnToAppend)
                 with open('./src/files/output/tst.json', 'w') as jsonf:
                     # json.dump(json.loads(json.dumps(clmnFull)), jsonf, indent=4, sort_keys=True)
-                    json.dump(clmnFull, jsonf, indent=4, sort_keys=True)
+                    json.dump(taskSimple, jsonf, indent=4)
                     jsonf.close()
 
     print(list(libraries))
--- src/tst_lists.py
+++ src/tst_lists.py
@@ -1,22 +1,11 @@
 if __name__ == '__main__':
-    dict = {'test_group' : ['b'], 'table_nm' : [], 'clmfull' : []}
-    first = {'a' : 10}
-    sec = {'name': 'C2C', 'length': '1;', 'type': '$', 'label': 'C2C'}
-    full = {'tst':[]}
-    tst = {'test_group' : ['b'], 'table_nm' : [], 'clmfull' : []}
-    dict['test_group'].append(first)
-    dict['test_group'].append(sec)
-    dict['table_nm'].append({'tablenm' : 'privet!'})
-    dict['table_nm'].append({'tablenm' : 'poka!'})
-    dict['clmfull'].append({'col1': 'col1asdsa!'})
-    dict['clmfull'].append({'col2': 'col2adafqr13!'})
-    full['tst'].append(dict)
-    print(full)
-    tst['test_group'].append(first)
-    tst['test_group'].append(sec)
-    tst['table_nm'].append({'tablenm' : '12312pr123ivet!'})
-    tst['table_nm'].append({'tablenm' : '12312po123ka!'})
-    tst['clmfull'].append({'col1': '123213col1123123asdsa!'})
-    tst['clmfull'].append({'col2': '123123co12312321l2adafqr13!'})
-    full['tst'].append(tst)
-    print(full)
+    match ' libname privet b  a="3"'.split():
+        case operator, libref, type_conn:
+            print(f'{operator=}, {libref=}, {type_conn=}')
+        case operator, libref, type_conn, path:
+            print(f'{operator=}, {libref=}, {type_conn=}, {path=}')
+            # print(len(path.split('"')))
+            for i in range(0, len(path.split('"'))):
+                print(path.split('"')[i])
+        case _:
+            print('wrong libname operator definition')
